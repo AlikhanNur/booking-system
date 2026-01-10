23527bf9f367429b29c8b8d9fb884f641b3ca655
Diyatt <120357743+Diyatt@users.noreply.github.com>
2026-01-11 00:02:13 +0500

    added models

--- internal/models/booking.go
+++ internal/models/booking.go
@@ -1,1 +1,20 @@
 package models
+
+import "time"
+
+// Booking — бронирование стола
+type Booking struct {
+	ID           int64 `json:"id"`
+	RestaurantID int64 `json:"restaurant_id"`
+	TableID      int64 `json:"table_id"`
+	UserID       int64 `json:"user_id"`
+
+	Date     time.Time `json:"date"`
+	TimeFrom time.Time `json:"time_from"`
+	TimeTo   time.Time `json:"time_to"`
+
+	Status string `json:"status"` // pending / confirmed / cancelled
+
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
--- internal/models/user.go
+++ internal/models/user.go
@@ -1,1 +1,14 @@
 package models
+
+import "time"
+
+// User — пользователь системы
+type User struct {
+	ID          int64     `json:"id"`
+	Name        string    `json:"name"`
+	Email       string    `json:"email"`
+	PhoneNumber string    `json:"phone_number"`
+	Role        string    `json:"role"` // user, admin, superadmin
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
